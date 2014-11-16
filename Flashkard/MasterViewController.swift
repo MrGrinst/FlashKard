ca46fbea3aae2b4f7dff62ebbbba756b2eec3a55
lukenewman <lukenewman@bellsouth.net>
2014-11-16 10:14:48 -0500

    Added New Notecard screen and adding deck code

--- Flashkard/MasterViewController.swift
+++ Flashkard/MasterViewController.swift
@@ -18,17 +18,53 @@ class MasterViewController: UITableViewController, UICollectionViewDataSource, U
         super.awakeFromNib()
     }
     
-    @IBAction func editDeck(sender: AnyObject) {
-        
+    @IBAction func newNote(sender: AnyObject) {
+        insertDeck()
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        decks = CoreDataManager.sharedInstance.fetchDecks().reverse()
+        arr = ["notecard.png","notecard.png", "notecard.png"]
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    func insertDeck() {
+        var inputTextField: UITextField?
+        let namePrompt = UIAlertController(title: "Deck Name", message: "What would you like to name this deck?", preferredStyle: UIAlertControllerStyle.Alert)
+        namePrompt.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: { (action) -> Void in
+            if (inputTextField!.text.stringByReplacingOccurrencesOfString(" ", withString: "") == "") {
+                self.presentViewController(namePrompt, animated: true, completion: nil)
+            } else {
+                var deck = CoreDataManager.sharedInstance.createNewDeck(inputTextField!.text)
+                self.decks.insert(deck, atIndex: 0)
+                let indexPath = NSIndexPath(forRow: 0, inSection: 0)
+                self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+            }
+        }))
+        namePrompt.addTextFieldWithConfigurationHandler({(textField: UITextField!) in
+            textField.placeholder = "Name"
+            inputTextField = textField
+        })
+        presentViewController(namePrompt, animated: true, completion: nil)
+    }
+    
+    func deleteDeck(indexPath: NSIndexPath) {
+        var deck = decks[indexPath.row]
+        let deleteConfirm = UIAlertController(title: "Delete '" + deck.name + "' deck?" , message: "Are you sure you want to delete this deck?", preferredStyle: UIAlertControllerStyle.Alert)
+        deleteConfirm.addAction(UIAlertAction(title: "No", style: UIAlertActionStyle.Default, handler: { (action) -> Void
+            in
+            self.tableView.reloadRowsAtIndexPaths([indexPath], withRowAnimation: .None)
+        }))
+        deleteConfirm.addAction(UIAlertAction(title: "Yes", style: UIAlertActionStyle.Default, handler: { (action) -> Void in
+            self.decks.removeAtIndex(indexPath.row)
+            CoreDataManager.sharedInstance.deleteDeck(deck)
+            self.tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        }))
+        presentViewController(deleteConfirm, animated: true, completion: nil)
     }
 
     // MARK: - Segues
