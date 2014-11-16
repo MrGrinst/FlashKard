ea6696dcf8a352d13fec867b557514905a40eddb
lukenewman <lukenewman@bellsouth.net>
2014-11-16 02:46:31 -0500

    committing something

--- Flashkard/MasterViewController.swift
+++ Flashkard/MasterViewController.swift
@@ -20,33 +20,15 @@ class MasterViewController: UITableViewController {
     @IBAction func editDeck(sender: AnyObject) {
         
     }
-    
-    
-    @IBAction func newNote(sender: AnyObject) {
-        
-        
-    }
-    
-    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.navigationItem.leftBarButtonItem = self.editButtonItem()
-
-        let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
-        self.navigationItem.rightBarButtonItem = addButton
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-
-    func insertNewObject(sender: AnyObject) {
-        objects.insertObject(NSDate(), atIndex: 0)
-        let indexPath = NSIndexPath(forRow: 0, inSection: 0)
-        self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
     }
 
     // MARK: - Segues
