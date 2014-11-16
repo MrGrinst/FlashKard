2aae239894a2592fa55877c4089e95e460d0b72b
anish92002 <LuckySharm@Anishs-MacBook-Pro.local>
2014-11-16 00:39:23 -0500

    added collectionview kinda...

---
+++ Flashkard/myViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  myViewCell.swift
+//  Flashkard
+//
+//  Created by Anish Sharma on 11/16/14.
+//  Copyright (c) 2014 Luke Newman. All rights reserved.
+//
+
+import UIKit
+
+class myViewCell: UICollectionViewCell {
+    
+}
